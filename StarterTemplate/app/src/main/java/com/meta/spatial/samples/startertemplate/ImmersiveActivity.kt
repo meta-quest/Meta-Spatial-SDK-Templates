4362207dc7aaa635b38eb88e4f68bbde0cdc21b0
Nivaldo Bondança <nbondanca@meta.com>
2026-08-02 08:20:26 -0700

    Update ktfmt component on FBS:master
    Reviewed By: cortinico
    
    Differential Revision: D112366935
    
    fbshipit-source-id: e0b509473d93d3fb4a465a3007bd0a7b63fa553d

--- StarterTemplate/app/src/main/java/com/meta/spatial/samples/startertemplate/ImmersiveActivity.kt
+++ StarterTemplate/app/src/main/java/com/meta/spatial/samples/startertemplate/ImmersiveActivity.kt
@@ -94,7 +94,7 @@ class ImmersiveActivity : AppSystemActivity() {
               unlit = true // Prevent scene lighting from affecting the skybox
             },
             Transform(Pose(Vector3(x = 0f, y = 0f, z = 0f))),
-        )
+        ),
     )
   }
 
